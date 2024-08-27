47ad250c19ae84106b204a379296ad2c1969b947
Abhijit Roy <dalvath3700@gmail.com>
2024-08-27 15:50:46 +0530

    Update heroku

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ This is a simple API written in Rust using the Axum framework. It is deployed on
 5. Create a buildpack: `heroku buildpacks:set emk/rust` for an existing application. For a new application, it can be specified while creating the app: `heroku create --buildpack emk/rust`. [Repo](https://github.com/emk/heroku-buildpack-rust).
 6. Verify info: `heroku info`
 7. Add heroku remote to git: `heroku git:remote -a <app-name>`. E.g. `heroku git:remote -a rust-api-heroku`
-8. Add Procfile: `web: ./target/release/rust-api-heroku`. `PORT=$PORT` is not required as it is already set by the Heroku. Just need to set the `.env` & configure the code to use the env vars using `dotenv` crate & `std::env::var` function.
+8. Add Procfile: `web: ./target/release/rust-api-heroku`. `PORT=$PORT` is not required as it is already set by the Heroku. Just need to set the `.env` & configure the code to use the env vars using `dotenv` crate & `std::env::var` function. So, you need to set the env vars in the Heroku app.
 9. Although optional to mention, but do create a `rust-toolchain.toml` file. It will help Heroku to know which version of Rust to use.
 10. Commit everything & push to Heroku: `git push heroku main`. For more, follow the [deployment instructions on Heroku](https://github.com/abhi3700/my_coding_toolkit/blob/main/heroku.md#deployment)
 11. Verify the app: `heroku open`
 12. Check logs: `heroku logs --tail`. It shows the real-time logs.
+
+[More](https://github.com/abhi3700/my_coding_toolkit/blob/main/heroku.md)
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,24 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
-    let port = std::env::var("PORT").expect("Failed to get the PORT env var.");
+	dotenv::dotenv().ok();
+	let port = std::env::var("PORT").expect("Failed to get the PORT env var.");
 
-    // depend on tracing for all sorts of logging
-    tracing_subscriber::fmt().init();
+	// depend on tracing for all sorts of logging
+	tracing_subscriber::fmt().init();
 
-    // build our router/application with single route
-    let app = Router::new()
-        .route("/", get(|| async { "Hello World!" }))
-        .route("/greet/", get(|| async { "Hey! glad to have you here!" }));
+	// build our router/application with single route
+	let app = Router::new()
+		.route("/", get(|| async { "Hello World!" }))
+		.route("/greet/", get(|| async { "Hey! glad to have you here!" }));
 
-    // define the TCP listener
-    let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{port}"))
-        .await
-        .unwrap();
+	// define the TCP listener
+	let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{port}")).await.unwrap();
 
-    tracing::debug!("Listening on port: {}", listener.local_addr().unwrap());
+	tracing::debug!("Listening on port: {}", listener.local_addr().unwrap());
 
-    // info!("API server started...");
+	// info!("API server started...");
 
-    // listen to/start the server
-    axum::serve(listener, app).await.unwrap();
+	// listen to/start the server
+	axum::serve(listener, app).await.unwrap();
 }
